630859f956a777f246a7f9aca396f1a7b16148ee
kai1812 <kai1812@gmail.com>
2017-03-07 21:16:11 +0800

    數字錯誤未做處理

---
+++ Example03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string sql =
+                @ "select * from table
+            where id >1000" ;
+            string xml =
+                @"<Pokemon>
+                      <NationalNo>#001</NationalNo>
+                      <Name>Bulbstaur</Name>
+                  </Pokemon> ;
+            string
+        }
+    }
+}
